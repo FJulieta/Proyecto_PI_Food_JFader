37e322ba169ef5d8ca29f199dbbdc58b4de423f0
Julieta Fader <julieta.fader@mercadolibre.com>
2023-07-28 10:08:08 -0300

    clearComments

--- client/src/components/Cards/Card.jsx
+++ client/src/components/Cards/Card.jsx
@@ -2,11 +2,7 @@ import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import s from './Card.module.css'
 
-//A continuacion lo que tenemos, es un componente llamado Card:
-//Este componente acepta una serie de props, como argumentos , y muestra una tarjeta con la información proporciona por estas props.
-//La card tiene un estructura div para poder aplicarle estilos, los cuales están importados a través de un objeto (s)
-//Proporciono un aimagen por defecto en el caso de que no se pueda obtener la info de la prop "imagen"
-//utilizo el componente proporcionado por react-router-dom, para el enrutamiento y navegar hacia el detail, el contenido de este enlace está condicionado por la prop showActions.
+//utilizo el componente "NavLink", proporcionado por react-router-dom, para el enrutamiento y navegar hacia el detail, el contenido de este enlace está condicionado por la prop showActions.
 function Card({ name, imagen, diets, id, showActions }) {
   return (
     <div className={s.cardContainer}>
